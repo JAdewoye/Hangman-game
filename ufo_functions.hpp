c47304a21086f7d807af3b3c466f263a533940a3
JAdewoye <104460005+JAdewoye@users.noreply.github.com>
2023-02-24 16:31:19 +0000

    Create ufo_functions.hpp
    functions declared

---
+++ ufo_functions.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+void display_misses(int misses);
+
+void display_status(std::vector<char> incorrect, std::string answer);
+
+void Greet();
+
+void end_game (std::string answer, std::string codeword);
+// Declare functions
